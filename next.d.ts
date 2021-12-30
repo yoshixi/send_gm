dfda59da653a3e37fe498b6add4075a0e6895fd7
yoshikimasubuchi <yoshixj4@gmail.com>
2021-12-30 16:28:11 +0900

    uodate

---
+++ next.d.ts
@@ -0,0 +1,26 @@
+import type {
+  NextComponentType,
+  NextPageContext,
+  NextLayoutComponentType,
+  NextPage,
+} from "next";
+import type { AppProps } from "next/app";
+
+declare module "next" {
+  type NextLayoutComponentType<P = {}> = NextComponentType<
+    NextPageContext,
+    any,
+    P
+  > & {
+    getLayout?: (page: ReactNode) => ReactNode;
+  };
+  type NextPage<P = {}, IP = P> = NextComponentType<NextPageContext, IP, P> & {
+    getLayout?: (page: ReactNode) => ReactNode;
+  };
+}
+
+declare module "next/app" {
+  type AppLayoutProps<P = {}> = AppProps & {
+    Component: NextLayoutComponentType;
+  };
+}
